493f0e75039685313609b7d91ede913756cb7c0a
Gabriel Jimenez <alexandro.jimenezg@gmail.com>
2021-04-28 14:37:09 -0500

    update ASM

--- gcc-calling-conventions/codes/effectASM.asm
+++ gcc-calling-conventions/codes/effectASM.asm
@@ -1,23 +1,30 @@
 global effectASM
-  section .text
+    section .text
 effectASM:
-  xor  r8, r8
-  xor  r9, r9
-  xor   rax, rax
+    xor r8, r8
+    xor r9, r9
+    xor rax, rax
+
+    ; rdi << *A
+    ; rsi << *B
+    ; rdx << *Casm
+    ; xmm0 << alpha
+    ; rcx << imgSize
+
 bucle:
-  cmp  ecx, r8d
-  jle  final
-  mov  r9b, BYTE [rsi+r8]
-  mov  al, BYTE [rdi+r8]
-  mov  r10b, r9b
-  cvtsi2ss  xmm2, r9
-  sub  r10b, al
-  cvtsi2ss  xmm1, r10
-  mulss  xmm1, xmm0
-  addss  xmm1, xmm2
-  cvttss2si  eax, xmm1
-  mov     [rdx+r8], al
-  inc  r8
-  jmp  bucle
+    cmp ecx, r8d
+    jle final
+    mov r9b, BYTE [rsi+r8]  ; B[i]
+    mov al, BYTE [rdi+r8]   ; A[i]
+    mov r10b, r9b
+    cvtsi2ss xmm2, r9   ; float(B[i])
+    sub r10b, al        ; (B[i]-A[i])
+    cvtsi2ss xmm1, r10  ; float(B[i]-A[i])
+    mulss xmm1, xmm0    ; alpha * float(B[i]-A[i])
+    addss xmm1, xmm2    ; float(B[i]) + alpha * float(B[i]-A[i])
+    cvttss2si eax, xmm1 ; uchar(float(B[i]) + alpha * float(B[i]-A[i]))
+    mov BYTE [rdx+r8], al    ; Casm[i]
+    inc r8
+    jmp bucle
 final:
-  ret
+    ret
