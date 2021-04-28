f6f1cb2919053810ea79abdf1ecc44af0c3cf57a
Gabriel Jimenez <alexandro.jimenezg@gmail.com>
2021-04-28 14:41:41 -0500

    update ASM

--- gcc-calling-conventions/codes/effectASM.asm
+++ gcc-calling-conventions/codes/effectASM.asm
@@ -17,13 +17,13 @@ bucle:
     mov r9b, BYTE [rsi+r8]  ; B[i]
     mov al, BYTE [rdi+r8]   ; A[i]
     mov r10b, r9b
-    cvtsi2ss xmm2, r9   ; float(B[i])
-    sub r10b, al        ; (B[i]-A[i])
-    cvtsi2ss xmm1, r10  ; float(B[i]-A[i])
-    mulss xmm1, xmm0    ; alpha * float(B[i]-A[i])
-    addss xmm1, xmm2    ; float(B[i]) + alpha * float(B[i]-A[i])
-    cvttss2si eax, xmm1 ; uchar(float(B[i]) + alpha * float(B[i]-A[i]))
-    mov BYTE [rdx+r8], al    ; Casm[i]
+    cvtsi2ss xmm2, r9       ; float(B[i])
+    sub r10b, al            ; (B[i]-A[i])
+    cvtsi2ss xmm1, r10      ; float(B[i]-A[i])
+    mulss xmm1, xmm0        ; alpha * float(B[i]-A[i])
+    addss xmm1, xmm2        ; float(B[i]) + alpha * float(B[i]-A[i])
+    cvttss2si eax, xmm1     ; uchar(float(B[i]) + alpha * float(B[i]-A[i]))
+    mov [rdx+r8], al      ; Casm[i]
     inc r8
     jmp bucle
 final:
