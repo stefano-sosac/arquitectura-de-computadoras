6c52e42f4826c07237060bc0c5081d60a560ee9a
Stefano Sosa <stefano.sosac@pucp.edu.pe>
2021-04-28 14:57:06 -0500

    fixing conflict

--- gcc-calling-conventions/codes/effectASM.asm
+++ gcc-calling-conventions/codes/effectASM.asm
@@ -18,7 +18,6 @@ bucle:
     mov r9b, [rsi+r8]  ; B[i]
     mov al, [rdi+r8]   ; A[i]
     mov r10b, r9b
-<<<<<<< HEAD
     cvtsi2ss xmm2, r9   ; float(B[i])
     sub r10b, al        ; (B[i]-A[i])
     cvtsi2ss xmm1, r10  ; float(B[i]-A[i])
@@ -26,7 +25,6 @@ bucle:
     addss xmm1, xmm2    ; float(B[i]) + alpha * float(B[i]-A[i])
     cvttss2si rax, xmm1 ; uchar(float(B[i]) + alpha * float(B[i]-A[i]))
     mov [rdx+r8], al    ; Casm[i]
-=======
     cvtsi2ss xmm2, r9       ; float(B[i])
     sub r10b, al            ; (B[i]-A[i])
     cvtsi2ss xmm1, r10      ; float(B[i]-A[i])
@@ -34,7 +32,6 @@ bucle:
     addss xmm1, xmm2        ; float(B[i]) + alpha * float(B[i]-A[i])
     cvttss2si eax, xmm1     ; uchar(float(B[i]) + alpha * float(B[i]-A[i]))
     mov [rdx+r8], al      ; Casm[i]
->>>>>>> f6f1cb2919053810ea79abdf1ecc44af0c3cf57a
     inc r8
     jmp bucle
 final:
