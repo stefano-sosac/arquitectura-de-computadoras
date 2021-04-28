5ccee7ee0877e9835d1b7f16fac701313b944fc6
Gabriel Jimenez <alexandro.jimenezg@gmail.com>
2021-04-28 15:12:01 -0500

    fix duplicate code

--- gcc-calling-conventions/codes/effectASM.asm
+++ gcc-calling-conventions/codes/effectASM.asm
@@ -25,13 +25,6 @@ bucle:
     addss xmm1, xmm2    ; float(B[i]) + alpha * float(B[i]-A[i])
     cvttss2si rax, xmm1 ; uchar(float(B[i]) + alpha * float(B[i]-A[i]))
     mov [rdx+r8], al    ; Casm[i]
-    cvtsi2ss xmm2, r9       ; float(B[i])
-    sub r10b, al            ; (B[i]-A[i])
-    cvtsi2ss xmm1, r10      ; float(B[i]-A[i])
-    mulss xmm1, xmm0        ; alpha * float(B[i]-A[i])
-    addss xmm1, xmm2        ; float(B[i]) + alpha * float(B[i]-A[i])
-    cvttss2si eax, xmm1     ; uchar(float(B[i]) + alpha * float(B[i]-A[i]))
-    mov [rdx+r8], al        ; Casm[i]
     inc r8
     jmp bucle
 final:
